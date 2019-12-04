19d25efdd9abcadcbf68af60e38e1d90ca6b2253
Vincent <vincentcoubray@gmail.com>
2019-12-04 23:59:25 +0100

    Day 3 - Part 1

--- src/main/kotlin/fr/vco/adventofcode/AdventOfCode3.kt
+++ src/main/kotlin/fr/vco/adventofcode/AdventOfCode3.kt
@@ -1,51 +1,69 @@
 package fr.vco.adventofcode
 
 import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
     val input = getInputReader("/inputAoC3.txt")
     val lines = input.readLines()
 
-    val s1 = Segment(Coord(5,1), Coord(5,6))
-    val s2 = Segment(Coord(4,3), Coord (6,3))
+    println(part1("R75,D30,R83,U83,L12,D49,R71,U7,L72",
+    "U62,R66,U55,R34,D71,R55,D58,R83" ))
+// = 159
 
-    println( s1.crossingCord(s2))
+    println(part1("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+        "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7" ))
+// = 135
 
-    val wire1 = Wire(lines[0])
-    val wire2 = Wire(lines[1])
+    println("Part 1 : ${part1(lines[0], lines[1])}")
 
+
+}
+
+fun part1( w1 : String, w2 : String):Int {
+    val wire1 = Wire(w1)
+    val wire2 = Wire(w2)
     val crossingsCoord = wire1.crossingCoords(wire2)
 
-    val dist = crossingsCoord.map { it.distance(Coord(0, 0)) }.min()
-
-    println(dist)
+    return crossingsCoord.map { it.distance(Coord(0, 0)) }.min()!!
 }
+
 
 data class Coord(val x: Int, val y :Int) {
     fun distance(other: Coord) = abs(this.x-other.x) + abs(this.y-other.y)
 }
 
 data class Segment(val start : Coord, val end :Coord) {
+    fun isHorizontal() = this.start.y - this.end.y == 0
+    fun isParallel(other : Segment) = this.isHorizontal() == other.isHorizontal()
 
-    fun crossingCord(other: Segment) : Coord? {
-        val dx = this.start.x - this.end.x
-        val dy = this.start.y - this.end.y
-        val odx = other.start.x - other.end.x
-        val ody = other.start.y - other.end.y
+    fun getIntersection(other: Segment) : Coord? {
 
-        val inter = if (dx == 0) {
-            if (odx == 0) return null
-            val range = IntRange(this.start.y, this.end.y)
-            if (range.contains(other.start.y)) Coord(this.start.x,other.start.y)
-            else null
-        } else if (dy == 0) {
-            if (ody == 0) return null
-            val range = IntRange(this.start.x, this.end.x)
-            if (range.contains(other.start.x)) Coord(other.start.x,this.start.y)
-            else null
-        } else return null
+        if (this.isParallel(other)) return null
+
+        val inter = if(this.isHorizontal()) {
+            getIntersection(this,other)
+        } else {
+            getIntersection(other,this)
+        }
+
+        println( "$this $other -> $inter")
         return if (inter != Coord(0,0)) inter else null
     }
+
+}
+
+
+fun getIntersection(hor :Segment , vert : Segment) : Coord? {
+    val x = vert.start.x
+    val y = hor.start.y
+    val firstY = min(vert.start.y, vert.end.y)
+    val lastY = max (vert.start.y, vert.end.y)
+    val firstX = min(hor.start.x, hor.end.x)
+    val lastX = max (hor.start.x, hor.end.x)
+    return if (y in firstY until lastY && x in firstX until lastX)  Coord(x,y)
+    else null
 }
 
 
@@ -76,15 +94,15 @@ class Wire() {
     private val segments = mutableListOf<Segment>()
 
     fun crossingCoords(other : Wire) : List<Coord>{
-        val crossingCoords = mutableListOf<Coord>()
+        val intersections = mutableListOf<Coord>()
         segments.forEach{s ->
             for (os in other.segments) {
 
-                val c = s.crossingCord(os)
-                if ( c != null) crossingCoords.add(c)
+                val c = s.getIntersection(os)
+                if ( c != null) intersections.add(c)
             }
         }
-        return crossingCoords
+        return intersections
     }
 
 }
