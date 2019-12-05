1adf915965d908a02bba2dbca49bbc90cf482947
vcoubray <vcoubray@sedona.fr>
2019-12-05 20:16:42 +0100

    Day 4 - Add Part 2

--- src/main/kotlin/fr/vco/adventofcode/AdventOfCode4.kt
+++ src/main/kotlin/fr/vco/adventofcode/AdventOfCode4.kt
@@ -4,24 +4,25 @@ fun main() {
 
     val start = 256310
     val end = 732736
-
     var countCode = 0
+    var countCode2 = 0
     for (code in start until end) {
-        if(code.isValid()) countCode++
+        if(code.isValid(false)) countCode++
+        if(code.isValid(true )) countCode2++
     }
     println ("Part 1 : $countCode")
+    println ("Part 2 : $countCode2")
 }
 
-
-fun Int.isValid() : Boolean {
+fun Int.isValid(strict : Boolean ) : Boolean {
     val code = this.toString()
     var prevDigit = 0
-    var hasDouble = false
     code.forEach {
         val digit = it.toInt()
         if (digit < prevDigit) return false
-        if (digit == prevDigit) hasDouble = true
         prevDigit = digit
     }
-    return hasDouble
+    return if(strict) code.groupBy{it}.filter{it.value.size == 2 }.isNotEmpty()
+    else code.groupBy{it}.filter{it.value.size >= 2 }.isNotEmpty()
 }
+
