298a22b21f359032991adac5e483b84c55c765ca
vcoubray <vcoubray@sedona.fr>
2019-12-05 19:42:38 +0100

    Day 3 - Add Part 2

--- src/main/kotlin/fr/vco/adventofcode/AdventOfCode3.kt
+++ src/main/kotlin/fr/vco/adventofcode/AdventOfCode3.kt
@@ -8,25 +8,28 @@ fun main() {
     val input = getInputReader("/inputAoC3.txt")
     val lines = input.readLines()
 
-    println(part1("R75,D30,R83,U83,L12,D49,R71,U7,L72",
-    "U62,R66,U55,R34,D71,R55,D58,R83" ))
-// = 159
+    solve("R75,D30,R83,U83,L12,D49,R71,U7,L72",
+    "U62,R66,U55,R34,D71,R55,D58,R83" )
+    // Part 1 = 159
+    // Part 2 = 610
 
-    println(part1("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
-        "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7" ))
-// = 135
+    solve("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+        "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7" )
+    // Part 1 = 135
+    // Part 2 = 410
 
-    println("Part 1 : ${part1(lines[0], lines[1])}")
+    solve(lines[0], lines[1])
 
 
 }
 
-fun part1( w1 : String, w2 : String):Int {
+fun solve(w1 : String, w2 : String){
     val wire1 = Wire(w1)
     val wire2 = Wire(w2)
     val crossingsCoord = wire1.crossingCoords(wire2)
 
-    return crossingsCoord.map { it.distance(Coord(0, 0)) }.min()!!
+    println("Part 1 : ${crossingsCoord.map { it.coord.distance(Coord(0, 0)) }.min()!!}")
+    println("Part 2 : ${crossingsCoord.minBy{it.steps}?.steps}")
 }
 
 
@@ -34,21 +37,17 @@ data class Coord(val x: Int, val y :Int) {
     fun distance(other: Coord) = abs(this.x-other.x) + abs(this.y-other.y)
 }
 
-data class Segment(val start : Coord, val end :Coord) {
+data class Segment(val start : Coord, val end :Coord, val length:Int) {
     fun isHorizontal() = this.start.y - this.end.y == 0
     fun isParallel(other : Segment) = this.isHorizontal() == other.isHorizontal()
 
     fun getIntersection(other: Segment) : Coord? {
-
         if (this.isParallel(other)) return null
-
         val inter = if(this.isHorizontal()) {
             getIntersection(this,other)
         } else {
             getIntersection(other,this)
         }
-
-        println( "$this $other -> $inter")
         return if (inter != Coord(0,0)) inter else null
     }
 
@@ -72,7 +71,6 @@ class Wire() {
     constructor(path :String) : this(){
         var origin= Coord(0,0)
         val paths = path.trim().split(",")
-
         paths.forEach{ p ->
             val dir = p[0]
             val dist = p.substring(1,p.length).toInt()
@@ -84,25 +82,33 @@ class Wire() {
                 'L' -> origin.copy(x = origin.x-dist)
                 else -> Coord(0,0)
             }
-            segments.add(Segment(origin,end))
+            segments.add(Segment(origin,end,dist))
             origin = end.copy()
         }
-        println(segments)
-
     }
 
     private val segments = mutableListOf<Segment>()
 
-    fun crossingCoords(other : Wire) : List<Coord>{
-        val intersections = mutableListOf<Coord>()
-        segments.forEach{s ->
-            for (os in other.segments) {
 
+    fun crossingCoords(other : Wire) : List<Intersection>{
+        val intersections = mutableListOf<Intersection>()
+        var dist1 = 0
+        segments.forEach{s ->
+            var dist2 = 0
+            for (os in other.segments) {
                 val c = s.getIntersection(os)
-                if ( c != null) intersections.add(c)
+                if ( c != null) {
+                    val steps = dist1 + c.distance(s.start) + dist2 + c.distance(os.start)
+                    intersections.add(Intersection(c,steps))
+                }
+                dist2+=os.length
             }
+            dist1 += s.length
         }
         return intersections
     }
 
 }
+
+
+data class Intersection(val coord: Coord, val steps : Int)
