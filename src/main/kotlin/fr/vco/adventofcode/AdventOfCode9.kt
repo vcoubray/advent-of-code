08364ade192db0b836d3c0c8a67749377fc67dcc
Vincent <vincentcoubray@gmail.com>
2019-12-12 20:09:57 +0100

    Day 9 - Part 2

--- src/main/kotlin/fr/vco/adventofcode/AdventOfCode9.kt
+++ src/main/kotlin/fr/vco/adventofcode/AdventOfCode9.kt
@@ -2,29 +2,29 @@ package fr.vco.adventofcode
 
 fun main() {
 
-    test ("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99".split(",").map { it.toLong() })
-    test ("1102,34915192,34915192,7,4,7,99,0".split(",").map { it.toLong() })
-    test ("104,1125899906842624,99".split(",").map { it.toLong() })
+    val test1 = execDay9 ("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99".split(",").map { it.toLong() })
+    val test2 = execDay9 ("1102,34915192,34915192,7,4,7,99,0".split(",").map { it.toLong() })
+    val test3 =  execDay9 ("104,1125899906842624,99".split(",").map { it.toLong() })
+    println("Test 1 : $test1")
+    println("Test 2 : $test2")
+    println("Test 3 : $test3")
 
     val input = getInputReader("/inputAoC9.txt")
     val intCode = input.readText().trim().split(",").map { it.toLong() }
-
-    test (intCode)
-
-
+    println("Part 1 : ${execDay9 (intCode,1)}")
+    println("Part 2 : ${execDay9 (intCode,2)}")
 
 }
 
 
-fun test(intCode : List<Long>){
-
+fun execDay9(intCode : List<Long>, input : Long? = null) : List<Long> {
     val stream = OpCodeStream()
     val opCode = OpCode(intCode, stream)
 
-
-    stream.input.add(1)
+    input?.let{stream.input.add(it)}
     while(!opCode.isEnded()) {
         opCode.exec()
     }
-    println(stream.output)
+    return stream.output
 }
+
