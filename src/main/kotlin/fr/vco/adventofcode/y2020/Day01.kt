ef2c9b56999180e23951577ae305983711f37823
vcoubray <vcoubray@sedona.fr>
2020-12-01 14:26:54 +0100

    2020 - Day 01 - Part 2

--- src/main/kotlin/fr/vco/adventofcode/y2020/Day01.kt
+++ src/main/kotlin/fr/vco/adventofcode/y2020/Day01.kt
@@ -6,10 +6,20 @@ import fr.vco.adventofcode.getInputReader
 fun main() {
     val input = getInputReader("/2020/inputDay01.txt")
     val expenses = input.readLines().map { it.toInt() }
-    expenses.forEachIndexed { i, a ->
-        expenses.subList(i, expenses.size).forEach { b ->
-            if (a + b == 2020) println("Result : ${a * b}")
-        }
-    }
+
+    println("Part 1 : ${find(expenses,2020,2)}")
+    println("Part 2 : ${find(expenses,2020,3)}")
+
 }
 
+fun find (expenses : List<Int>, total :Int , count: Int) :Int{
+    if (count == 1 && expenses.contains(total)) return total
+    if (count >= 1) expenses.forEachIndexed{ i, it ->
+        val search = total - it
+        if(search >0) {
+            val result = find(expenses.subList(i, expenses.size),search, count-1)
+            if( result > 0 ) return result * it
+        }
+    }
+    return 0
+}
