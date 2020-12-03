80275a25a8ddec64a21ce3c2c2c11679ea3806a7
Vincent <vincentcoubray@gmail.com>
2020-12-03 19:47:48 +0100

    2020 - Day 02 - clean code

--- src/main/kotlin/fr/vco/adventofcode/y2020/Day02.kt
+++ src/main/kotlin/fr/vco/adventofcode/y2020/Day02.kt
@@ -5,20 +5,19 @@ import fr.vco.adventofcode.getInputReader
 fun main() {
     val input = getInputReader("/2020/inputDay02.txt")
     val lines = input.readLines()
-    val passwords = lines.map{ it ->
+    val passwords = lines.map{
         val (range, letter, password) = it.split(" ")
         val (min, max) = range.split("-").map{r -> r.toInt()}
-        password(min, max, letter.first(), password)
+        Password(min, max, letter.first(), password)
     }
 
-    println("Part 1 : ${passwords.filter{it.isValid()}.size}")
-    println("Part 2 : ${passwords.filter{it.isValidPart2()}.size}")
+    println("Part 1 : ${passwords.count{it.isValid()}}")
+    println("Part 2 : ${passwords.count{it.isValidPart2()}}")
 
 }
 
-data class password(val min :Int, val max: Int,val letter :Char, val password :String ) {
+data class Password(val min :Int, val max: Int,val letter :Char, val password :String ) {
 
     fun isValid() = password.count{it == letter} in min .. max
-
     fun isValidPart2() = (password[min-1] == letter) xor (password[max-1] == letter)
 }
