3ae26ae02247d8f902a9c8b0bb2ce8cd775add47
Vincent <vincentcoubray@gmail.com>
2020-12-06 12:23:51 +0100

    2020 - Day 06 - Part 2

--- src/main/kotlin/fr/vco/adventofcode/y2020/Day06.kt
+++ src/main/kotlin/fr/vco/adventofcode/y2020/Day06.kt
@@ -6,12 +6,13 @@ fun main() {
     val input = getInputReader("/2020/inputDay06.txt")
     val lines = input.readLines()
 
-    val groups = mutableListOf(mutableSetOf<Char>())
-
-    lines.forEach{
-        if(it.isBlank())groups.add(mutableSetOf())
-        else groups.last().addAll(it.toCharArray().toList())
+    val groups = mutableListOf(mutableListOf<Set<Char>>())
+    lines.forEach {
+        if (it.isBlank()) groups.add(mutableListOf())
+        else groups.last().add(it.toSet())
     }
 
-    println("Part 1 : ${groups.sumBy{it.size}}")
+    println("Part 1 : ${groups.sumBy { it.reduce { a, b -> a union b }.size }}")
+    println("Part 2 : ${groups.sumBy { it.reduce { a, b -> a intersect b }.size }}")
+
 }
