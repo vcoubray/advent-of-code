d7be34b3aac93ddd6995d132defc4921afd66824
vcoubray <vcoubray@sedona.fr>
2020-12-08 14:13:50 +0100

    2020 - Day 08 - Part 1 & 2 - Input

---
+++ src/main/kotlin/fr/vco/adventofcode/y2020/Day08.kt
@@ -0,0 +1,59 @@
+package fr.vco.adventofcode.y2020
+
+import fr.vco.adventofcode.getInputReader
+
+fun main() {
+    val input = getInputReader("/2020/inputDay08.txt")
+    val lines = input.readLines()
+
+    val code = BootCode(lines.map { it.split(" ") }.map { it.first() to it.last().toInt() })
+
+    println("Part 1 : ${code.exec()}")
+    println("Part 2 : ${code.execWithoutLoop()}")
+}
+
+
+class BootCode(private val instructions: List<Pair<String, Int>>) {
+    private var acc: Int = 0
+    private var cursor: Int = 0
+    private var swapIndex: Int = -1
+
+    fun execWithoutLoop() : Int {
+        swapIndex = 0
+        while (looping()) {
+            if (instructions[swapIndex].first != "acc") {
+                exec()
+            }
+            swapIndex++
+        }
+        return acc
+    }
+
+
+    fun exec() : Int{
+        reset()
+        val visited = mutableSetOf<Int>()
+        while (!visited.contains(cursor) && cursor < instructions.size) {
+            visited.add(cursor)
+            exec(instructions[cursor])
+        }
+        return acc
+    }
+
+    private fun reset() {
+        acc = 0
+        cursor = 0
+    }
+
+    private fun looping() = cursor != instructions.size
+
+    private fun exec(instruction: Pair<String, Int>) {
+        val (ins, n) = instruction
+        when {
+            ins == "acc" -> { acc += n; cursor++ }
+            (ins ==  "jmp" && swapIndex != cursor) ||
+            (ins ==  "nop" && swapIndex == cursor) -> cursor += n
+            else -> cursor++
+        }
+    }
+}
