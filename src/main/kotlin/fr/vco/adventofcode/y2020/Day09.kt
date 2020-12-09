f5f9c658f6d4b999dedf281998c2dc473934ce61
vcoubray <vcoubray@sedona.fr>
2020-12-09 19:48:14 +0100

    2020 - Day 09 - Part 2

--- src/main/kotlin/fr/vco/adventofcode/y2020/Day09.kt
+++ src/main/kotlin/fr/vco/adventofcode/y2020/Day09.kt
@@ -2,24 +2,39 @@ package fr.vco.adventofcode.y2020
 
 import fr.vco.adventofcode.getInputReader
 
+const val PREAMBLE_SIZE = 25
+
 fun main() {
     val input = getInputReader("/2020/inputDay09.txt")
     val lines = input.readLines().map { it.toLong() }
 
-    for (i in 25 until lines.size) {
-        if (!lines[i].isValid(lines.subList(i - 25, i).sorted())) {
-            println("Part 1 :${lines[i]}")
-            break
+    lines.drop(PREAMBLE_SIZE).forEachIndexed{i, it ->
+        if (!it.isValid(lines.subList(i, i+PREAMBLE_SIZE))) {
+            println("Part 1 : $it")
+            val weakness = it.getWeakness(lines).sorted()
+            if(weakness.isNotEmpty()) {
+                println("Part 2 : ${weakness.first() + weakness.last()}")
+            }
+            return@forEachIndexed
         }
     }
 }
 
 fun Long.isValid(preamble: List<Long>): Boolean {
-    if (this !in preamble[0] + preamble[1]..preamble.last() + preamble[preamble.size - 2]) return false
     preamble.forEachIndexed { i, it ->
-        if (it > this) return false
         if (preamble.drop(i).any { n -> it + n == this }) return true
     }
     return false
+}
+
+fun Long.getWeakness(preamble: List<Long>): List<Long>{
+    for(i in 0..preamble.size) {
+        for(j in i..preamble.size) {
+            val sum = preamble.subList(i,j).sum()
+            if(sum == this) return preamble.subList(i,j)
+            if(sum > this) break
+        }
+    }
+    return emptyList()
 }
 
