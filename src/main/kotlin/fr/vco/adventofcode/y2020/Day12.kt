96e1cf3c5820dcf5e6fe704cee6cb27e7d9cb9a9
Vincent <vincentcoubray@gmail.com>
2020-12-12 22:43:43 +0100

    2020 - Day 12 - Improve code again

--- src/main/kotlin/fr/vco/adventofcode/y2020/Day12.kt
+++ src/main/kotlin/fr/vco/adventofcode/y2020/Day12.kt
@@ -7,14 +7,8 @@ fun main() {
     val input = getInputReader("/2020/inputDay12.txt")
     val lines = input.readLines()
 
-    val boat = Boat()
-    lines.forEach(boat::move)
-    println("Part 1 : ${boat.dist()}")
-
-
-    val boatPart2 = Boat(waypoint = Position(10, 1))
-    lines.forEach { boatPart2.move(it, boatPart2::moveWaypoint) }
-    println("Part 2 : ${boatPart2.dist()}")
+    println("Part 1 : ${lines.fold(Boat()){ boat, it -> boat.move(it)}.dist(ORIGIN)}")
+    println("Part 2 : ${lines.fold(Boat(waypoint = Position(10, 1))){ boat, it -> boat.move(it,boat::moveWaypoint)}.dist(ORIGIN)}")
 }
 
 fun Int.toRadian() = this * PI / 180
@@ -34,29 +28,28 @@ val WEST = Position(-1, 0)
 val NORTH = Position(0, 1)
 val SOUTH = Position(0, -1)
 
-class Boat(
-    var position: Position = ORIGIN,
-    var waypoint: Position = EAST,
+data class Boat(
+    val position: Position = ORIGIN,
+    val waypoint: Position = EAST,
 ) {
 
-    fun move(instruction: String, move: (Position, Int) -> Unit = ::moveBoat) {
+    fun move(instruction: String, move: (Position, Int) -> Boat = ::moveBoat): Boat {
         val ins = instruction.first()
         val number = instruction.drop(1).toInt()
-        when (ins) {
+        return when (ins) {
             'N' -> move(NORTH, number)
             'S' -> move(SOUTH, number)
             'E' -> move(EAST, number)
             'W' -> move(WEST, number)
-            'F' -> position = waypoint * number + position
-            'L' -> waypoint = waypoint.turn(number.toRadian())
-            'R' -> waypoint = waypoint.turn(-number.toRadian())
+            'F' -> this.copy(position = waypoint * number + position)
+            'L' -> this.copy(waypoint = waypoint.turn(number.toRadian()))
+            'R' -> this.copy(waypoint = waypoint.turn(-number.toRadian()))
             else -> error("Should not append")
         }
     }
 
-    fun moveBoat(direction: Position, factor: Int) { position = direction * factor + position }
-    fun moveWaypoint(direction: Position, factor: Int) { waypoint = direction * factor + waypoint }
+    fun moveBoat(direction: Position, factor: Int) = this.copy( position = direction * factor + position )
+    fun moveWaypoint(direction: Position, factor: Int) = this.copy( waypoint = direction * factor + waypoint )
 
-    fun dist() = position.dist(ORIGIN)
+    fun dist(position : Position = ORIGIN) = this.position.dist(position)
 }
-
