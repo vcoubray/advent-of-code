23b86522d0b0c151328b7046db4178a1bbc9fd90
Vincent <vincentcoubray@gmail.com>
2020-12-14 22:04:29 +0100

    2020 - Day 14 - improve code

--- src/main/kotlin/fr/vco/adventofcode/y2020/Day14.kt
+++ src/main/kotlin/fr/vco/adventofcode/y2020/Day14.kt
@@ -6,62 +6,56 @@ fun main() {
     val input = getInputReader("/2020/inputDay14.txt")
     val lines = input.readLines()
 
-    lateinit var mask : Mask
-    val mem = mutableMapOf<Int,Long>()
-    lines.map{it.split(" = ")}
-        .forEach{ (ins, value) ->
-            if(ins == "mask") {
-                mask = Mask(value)
-            }else {
-                val addr = ins.drop(4).dropLast(1).toInt()
-                mem[addr] = value.toLong().applyMask(mask)
-            }
+    lines.fold(Memory()) { mem, ins -> mem.applyInstruction(ins, mem::applyValueMask) }
+        .run { println("Part 1 : ${mem.values.sum()}") }
+
+    lines.fold(Memory()) { mem, ins -> mem.applyInstruction(ins, mem::applyAddrMask) }
+        .run { println("Part 2 : ${mem.values.sum()}") }
+}
+
+class Memory {
+    lateinit var mask: Mask
+    val mem = mutableMapOf<Long, Long>()
+
+    fun applyInstruction(instruction: String, applyMask: (Long, Long, Mask) -> Unit) = apply {
+        val (ins, value) = instruction.split(" = ")
+        if (ins == "mask") {
+            mask = Mask(value)
+        } else {
+            val addr = ins.drop(4).dropLast(1).toLong()
+            applyMask(addr, value.toLong(), mask)
         }
+    }
 
-    println("Part 1 : ${mem.values.sum()}")
+    fun applyValueMask(addr: Long, value: Long, mask: Mask) {
+        mem[addr] = mask.applyValueMask(value)
+    }
 
-
-    val mem2 = mutableMapOf<Long,Long>()
-    lateinit var addrMask : AddrMask
-    lines.map{it.split(" = ")}
-        .forEach{ (ins, value) ->
-            if(ins == "mask") {
-                addrMask = AddrMask(value)
-            }else {
-                val addr = ins.drop(4).dropLast(1).toLong()
-                addr.applyAddrMask(addrMask).forEach{
-                    mem2[it] = value.toLong()
-                }
-            }
+    fun applyAddrMask(addr: Long, value: Long, mask: Mask) {
+        mask.applyAddrMask(addr).forEach {
+            mem[it] = value
         }
-    println("Part 2 : ${mem2.values.sum()}")
-
+    }
 }
 
-
-fun Long.applyMask(mask: Mask) = this and mask.mask0 or mask.mask1
-
-fun Long.applyAddrMask(mask : AddrMask ) : List<Long> {
-    val masked = (this or mask.mask1).toString(2).padStart(36, '0')
-    return mask.floatingBits
-        .fold(listOf(masked)){ acc, i -> acc.map{it.applyFloatingBit(i)}.flatten() }
-        .map{it.toLong(2)}
-}
-
-fun String.applyFloatingBit(index : Int) = listOf(
-    String(this.toCharArray().also{it[index] = '0' }),
-    String(this.toCharArray().also{it[index] = '1' })
-)
-
-class Mask(mask: String ) {
-    val mask0: Long = mask.replace("X", "1").toLong(2)
-    val mask1: Long = mask.replace("X", "0").toLong(2)
-}
-
-class AddrMask(mask : String) {
-    val mask1: Long = mask.replace("X", "0").toLong(2)
-    val floatingBits = mask.mapIndexed{i, it -> it to i}
-        .filter{(c,_) -> c == 'X'}
-        .map{(_,index) -> index}
+class Mask(mask: String) {
+    private val mask0: Long = mask.replace("X", "1").toLong(2)
+    private val mask1: Long = mask.replace("X", "0").toLong(2)
+    private val floatingBits = mask.mapIndexed { i, it -> it to i }
+        .filter { (c, _) -> c == 'X' }
+        .map { (_, index) -> index }
         .toList()
+
+    fun applyValueMask(value: Long) = value and mask0 or mask1
+    fun applyAddrMask(addr: Long): List<Long> {
+        val masked = (addr or mask1).toString(2).padStart(36, '0')
+        return floatingBits
+            .fold(listOf(masked)) { acc, i -> acc.map { it.applyFloatingBit(i) }.flatten() }
+            .map { it.toLong(2) }
+    }
 }
+
+fun String.applyFloatingBit(index: Int) = listOf(
+    String(this.toCharArray().also { it[index] = '0' }),
+    String(this.toCharArray().also { it[index] = '1' })
+)
