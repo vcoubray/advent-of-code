499a201dfa02bf98124e97bbd3f5727629a4d35b
vcoubray <vcoubray@sedona.fr>
2020-12-18 11:09:39 +0100

    2020 - Day 17 - Part 2

--- src/main/kotlin/fr/vco/adventofcode/y2020/Day17.kt
+++ src/main/kotlin/fr/vco/adventofcode/y2020/Day17.kt
@@ -7,118 +7,103 @@ fun main() {
     val lines = input.readLines()
 
     val area = Area().apply { init(lines) }
-    area.print()
-
-
-
-    println("Part 1 :${area.cycleUntil(6).count()}")
-
+    println("Part 1 :${area.cycleUntil(6){it.expand()}.count()}")
+    println("Part 2 :${area.cycleUntil(6){it.expand4D()}.count()}")
 
 }
 
+data class Cube(val x: Int, val y: Int, val z: Int = 0, val w: Int = 0) {
 
-class Area(val cubes : MutableMap<Int, MutableMap<Int, MutableMap<Int, Boolean>>> = mutableMapOf()) {
+    fun getNeighbours(): List<Cube> {
+        val neighbours = mutableListOf<Cube>()
+        for (wi in w - 1..w + 1) for (zi in z - 1..z + 1) for (yi in y - 1..y + 1) for (xi in x - 1..x + 1) {
+            val cube = Cube(xi, yi, zi, wi)
+            if (cube != this) neighbours.add(cube)
+        }
+        return neighbours
+    }
+}
+
+class Area(val cubes: MutableMap<Cube, Boolean> = mutableMapOf()) {
 
     lateinit var height: IntRange // z
     lateinit var depth: IntRange // y
     lateinit var width: IntRange // x
-
-
+    lateinit var hyperspace: IntRange // w
 
     fun init(area: List<String>) {
+        hyperspace = 0..0
         height = 0..0
         depth = area.indices
         width = area.first().indices
 
-        cubes[0] = mutableMapOf()
         area.forEachIndexed { y, line ->
-            cubes[0]!![y] = line.mapIndexed { x, it -> x to (it == '#') }.toMap().toMutableMap()
+            line.mapIndexed { x, it -> Cube(x, y) to (it == '#') }
+                .forEach { (cube, activate) -> cubes[cube] = activate }
         }
     }
 
-    fun getCube(x: Int, y: Int, z: Int): Boolean {
-        if(x !in width || y !in depth || z !in height) return false
-        return cubes[z]!![y]!![x]!!
-    }
-
-    fun createCubeIfNotExist(x: Int, y: Int, z: Int, active : Boolean = false) {
-        if (!cubes.containsKey(z)) cubes[z] = mutableMapOf()
-        if (!cubes[z]!!.containsKey(y)) cubes[z]!![y] = mutableMapOf()
-        if (!cubes[z]!![y]!!.containsKey(x)) cubes[z]!![y]!![x] = active
-    }
-
-    fun getNeighbours(x :Int, y : Int, z: Int) : List<Boolean> {
-        val neighbours = mutableListOf<Boolean>()
-        for(i in z-1..z+1) for (j in y-1..y+1) for (k in x-1..x+1) {
-            if(i != z || j !=y || k != x)
-                neighbours.add(getCube(k,j,i))
-        }
-        return neighbours
-    }
-
+    fun getCube(cube: Cube) =
+        if (!cubes.containsKey(cube)) false
+        else cubes[cube]!!
 
     fun expand(): Area {
         val area = Area()
         area.height = height.expand(1)
         area.depth = depth.expand(1)
         area.width = width.expand(1)
-
-        for(z in area.height)
+        area.hyperspace = hyperspace
+        for (z in area.height)
             for (y in area.depth)
                 for (x in area.width)
-                    area.createCubeIfNotExist(x,y,z,false)
+                    area.cubes[Cube(x, y, z)] = false
+        return area
+    }
+
+    fun expand4D(): Area {
+        val area = Area()
+        area.height = height.expand(1)
+        area.depth = depth.expand(1)
+        area.width = width.expand(1)
+        area.hyperspace = hyperspace.expand(1)
+
+        for (w in area.hyperspace)
+            for (z in area.height)
+                for (y in area.depth)
+                    for (x in area.width)
+                        area.cubes[Cube(x, y, z,w)] = false
         return area
 
     }
 
-    fun IntRange.expand(expansion: Int) = this.first-expansion..this.last+expansion
+    fun IntRange.expand(expansion: Int) = this.first - expansion..this.last + expansion
 
-    fun cycle(): Area {
-        val area = expand()
-        area.cubes.forEach{(z,plan) ->
-            plan.forEach{(y,line) ->
-                line.forEach{(x, _) ->
-                    val cube = getCube(x,y,z)
-                    val neighbours = getNeighbours(x,y,z)
-                    when {
-                        cube && neighbours.count{it} in 2..3 -> area.cubes[z]!![y]!![x] = true
-                        cube && neighbours.count{it} !in 2..3 -> area.cubes[z]!![y]!![x] = false
-                        !cube && neighbours.count{it} == 3 -> area.cubes[z]!![y]!![x] = true
-                        !cube && neighbours.count{it} != 3 -> area.cubes[z]!![y]!![x] = false
-                        else -> area.cubes[z]!![y]!![x] = cube
-                    }
-                }
+    fun cycle(expandMethod : (Area) -> Area): Area {
+        val area = expandMethod(this)
+        for (w in area.hyperspace) for (z in area.height) for (y in area.depth) for (x in area.width) {
+            val cube = Cube(x, y, z,w)
+            val neighbours = cube.getNeighbours().map { getCube(it) }
+            val cubeState = getCube(cube)
+            when {
+                cubeState && neighbours.count { it } in 2..3 -> area.cubes[cube] = true
+                cubeState && neighbours.count { it } !in 2..3 -> area.cubes[cube] = false
+                !cubeState && neighbours.count { it } == 3 -> area.cubes[cube] = true
+                !cubeState && neighbours.count { it } != 3 -> area.cubes[cube] = false
+                else -> area.cubes[cube] = cubeState
             }
         }
         return area
     }
-    fun cycleUntil(turn :Int) : Area{
+
+    fun cycleUntil(turn: Int,expandMethod : (Area) -> Area ): Area {
         var area = this
-        repeat(turn){
-            area = area.cycle()
-            area.print()
+        repeat(turn) {
+            area = area.cycle(expandMethod)
         }
         return area
     }
 
-
-    fun count(): Int{
-        return cubes.map{it.value.map{n-> n.value.map{m -> m.value}}}.flatten().flatten().count{ it }
-    }
-
-    fun print() {
-        cubes.forEach{ (z,plan) ->
-            println("Z = ${z}")
-            plan.forEach { (y, line) ->
-                line.forEach { (x, cube) ->
-                    print(if(cube)"#" else ".")
-                }
-                println()
-            }
-            println()
-        }
-        println()
-    }
+    fun count() = cubes.count { it.value }
 }
 
 
