80323d356f2bc580d21efabdaca0c0bda6799618
Vincent <vincentcoubray@gmail.com>
2020-12-21 10:12:55 +0100

    2020 - Day 19 - Part 2

--- src/main/kotlin/fr/vco/adventofcode/y2020/Day19.kt
+++ src/main/kotlin/fr/vco/adventofcode/y2020/Day19.kt
@@ -6,32 +6,28 @@ fun main() {
     val input = getInputReader("/2020/inputDay19.txt")
     val lines = input.readLines()
 
-    val(rules, messages) = lines.filterNot{it.isBlank()}.partition{"(\\d)+:.*".toRegex().matches(it)}
-    val rulesMap = rules.map { it.split(": ") }.map { (id,rule) -> id.toInt() to Rule(rule) }.toMap()
+    val (rulesExp, messages) = lines.filterNot { it.isBlank() }.partition { "(\\d)+:.*".toRegex().matches(it) }
+    val rules = rulesExp.map { it.split(": ") }.map { (id, rule) -> id.toInt() to Rule(rule) }.toMap()
 
-    println("Part 1 :${messages.count(rulesMap.getRegex(0).toRegex()::matches)}")
+    println("Part 1 :${messages.count(rules.getRegex(0).toRegex()::matches)}")
+
+    rules[8]?.exp = List(10){List(it+1){42}.joinToString(" ")}.joinToString(" | ")
+    rules[11]?.exp = List(10){(List(it+1){42}+List(it+1){31}).joinToString(" ")}.joinToString(" | ")
+    println("Part 2 :${messages.count(rules.getRegex(0).toRegex()::matches)}")
 
 }
 
-fun Map<Int,Rule>.getRegex(id: Int): String{
-    return this[id]?.let{
-        if(it.regex == null) {
-            it.regex = it.tree.joinToString(separator="|", prefix = "(", postfix = ")"){
-                    ids-> ids.joinToString (""){ id -> this.getRegex(id) }
-            }
+fun Map<Int, Rule>.getRegex(id: Int): String {
+    return this[id]?.let { rule->
+        if ("\"\\w\"".toRegex().matches(rule.exp)) rule.exp[1].toString()
+        else {
+            rule.exp.split(" | ")
+                .map { ids -> ids.split(" ").map { it.toInt() } }
+                .joinToString(separator = "|", prefix = "(", postfix = ")") { ids ->
+                   ids.joinToString("") { id -> this.getRegex(id) }
+                }
         }
-        it.regex
-    }?:""
+    } ?: ""
 }
 
-
-class Rule(rule: String) {
-
-    lateinit var tree : List<List<Int>>
-    var regex : String? = null
-    init{
-        if ("\"\\w\"".toRegex().matches(rule)) regex = rule[1].toString()
-        else tree = rule.split(" | ").map{it.split(" ").map{id  -> id.toInt()}}
-    }
-
-}
+data class Rule(var exp: String)
