364b83e23364be2101c3eb4076cc34b933dec19a
Vincent <vincentcoubray@gmail.com>
2020-12-23 20:41:56 +0100

    2020 - Day 21 - Part 2

--- src/main/kotlin/fr/vco/adventofcode/y2020/Day21.kt
+++ src/main/kotlin/fr/vco/adventofcode/y2020/Day21.kt
@@ -27,8 +27,16 @@ fun main() {
     foods.keys.flatten().count{!allergenIngredients.contains(it)}
         .let{println("Part 1 : $it")}
 
+    val dangerousIngredients = mutableMapOf<String,String>()
+    allergens.forEach(::println)
 
+    while (allergens.any{(key,_)-> !dangerousIngredients.containsKey(key) }) {
+        allergens.map{(key,value)-> key to value.minus(dangerousIngredients.values)}
+            .filter{(_, value)-> value.size == 1}
+            .forEach{ (key, value) -> dangerousIngredients[key] = value.first()}
+    }
 
+    dangerousIngredients.keys.sorted()
+        .joinToString (","){dangerousIngredients[it]!!}
+        .let{println("Part 2 : $it")}
 }
-
-
