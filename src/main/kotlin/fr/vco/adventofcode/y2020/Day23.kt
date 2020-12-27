c6b2813bd66db64dd816bd8c26f86e82131f88dc
Vincent <vincentcoubray@gmail.com>
2020-12-27 19:16:32 +0100

    2020 - Day 23 - Part 2

--- src/main/kotlin/fr/vco/adventofcode/y2020/Day23.kt
+++ src/main/kotlin/fr/vco/adventofcode/y2020/Day23.kt
@@ -1,60 +1,94 @@
 package fr.vco.adventofcode.y2020
 
 import fr.vco.adventofcode.getInputReader
-import java.util.*
+import kotlin.system.measureTimeMillis
 
 fun main() {
 
     val input = getInputReader("/2020/inputDay23.txt")
-    val lines = input.readLines()
+    val line = input.readLines().first().map { "$it".toInt() }
 
-    val cups = LinkedList(lines.first().map{"$it".toInt()})
+    println("Part 1 : ${CupCircle(line).move(100).printPart1()}")
 
-    println("Part 1 : ${(0..99).fold(CupCircle(cups,0)){acc, _ -> acc.move()}}")
-
+    measureTimeMillis {
+        println("Part 2 : ${CupCircle(line, 1_000_000).move(10_000_000).printPart2()}")
+    }.let { println("in ${it}ms") }
 
 }
 
+class CupCircle(startCups: List<Int>, totalCups: Int = startCups.size) {
+    private val cups: MutableList<Int> = MutableList(totalCups + 1) { 0 }
+    private var current = cups.first()
+    private val pickup = mutableListOf<Int>()
 
-data class CupCircle(val cups: LinkedList<Int>, val currentIndex: Int) {
-
-
-    fun move() : CupCircle{
-
-        val current = cups.getClockwise(currentIndex)
-
-        // pickUp 3 cups
-        val pickup = mutableListOf<Int>()
-        val newCircle = LinkedList(cups)
-        for (i in (currentIndex + 1)..currentIndex + 3) {
-            pickup.add(cups.getClockwise(i))
+    init {
+        cups[0] = startCups.first()
+        for (i in 1 until totalCups) {
+            when {
+                i < startCups.size -> cups[startCups[i - 1]] = startCups[i]
+                i == startCups.size -> cups[startCups[i - 1]] = i + 1
+                else -> cups[i] = i + 1
+            }
         }
-        newCircle.removeAll(pickup)
-        // select destination
-        val dest = newCircle.filter { it < current }
-            .maxByOrNull { it }
-            ?: newCircle.filterNot { it == current }
-                .maxByOrNull { it }!!
-        val destIndex = newCircle.indexOf(dest)
-
-        // place picked up cups
-        pickup.forEachIndexed{i, it ->
-            newCircle.add(destIndex+1+i,it)
+        if (totalCups <= startCups.size) {
+            cups[startCups.last()] = startCups.first()
+        } else {
+            cups[totalCups] = startCups.first()
         }
-
-        return CupCircle(newCircle, newCircle.getClockwiseIndex(newCircle.indexOf(current)+1))
     }
 
-    override fun toString() =
+    private fun nextCurrent() {
+        current = cups[current]
+    }
+
+    private fun getDest(): Int {
+        var dest = current
+        do {
+            if (--dest == 0) dest = cups.size - 1
+        } while (pickup.contains(dest))
+        return dest
+    }
+
+    private fun pickup(size: Int = 3) {
+        var pick = current
+        repeat(size) {
+            pickup.add(cups[pick])
+            pick = cups[pick]
+        }
+        cups[current] = cups[pick]
+    }
+
+    private fun pushPickup(dest: Int) {
+        val destLink = cups[dest]
+        cups[dest] = pickup.first()
+        cups[pickup.last()] = destLink
+        pickup.clear()
+    }
+
+    private fun move() {
+        pickup()
+        val dest = getDest()
+        pushPickup(dest)
+        nextCurrent()
+    }
+
+    fun move(turn: Int) = apply {
+        repeat(turn) { move() }
+    }
+
+    fun printPart1() =
         StringBuffer().apply {
-            val index = cups.indexOf(1)
-            for (i in 1 until cups.size) {
-                this.append(cups.getClockwise( index  + i))
+            var index = cups[1]
+            while (index != 1) {
+                append(index)
+                index = cups[index]
             }
         }.toString()
 
+    fun printPart2(): Long {
+        val a1 = cups[1]
+        val a2 = cups[a1]
+        return a1.toLong() * a2.toLong()
+    }
+
 }
-
-fun LinkedList<Int>.getClockwiseIndex(index: Int) = (index % this.size).let{if(it >=0 ) it else this.size+it }
-fun LinkedList<Int>.getClockwise(index: Int) = this[this.getClockwiseIndex(index)]
-
