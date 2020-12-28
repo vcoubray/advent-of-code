28e34d57dbadb046d57499b7fdbc12e02202d3d0
Vincent <vincentcoubray@gmail.com>
2020-12-28 17:05:21 +0100

    2020 - Day 25 - END

---
+++ src/main/kotlin/fr/vco/adventofcode/y2020/Day25.kt
@@ -0,0 +1,44 @@
+package fr.vco.adventofcode.y2020
+
+import fr.vco.adventofcode.getInputReader
+
+
+fun main(){
+    val input = getInputReader("/2020/inputDay25.txt")
+    val lines = input.readLines()
+    val (cardKey, doorKey ) = lines.map{Key(it.toInt())}
+
+    println("Part 1 : ${cardKey.encrypt(doorKey)}")
+
+}
+
+class Key(private val publicKey: Int) {
+
+    private companion object{
+        const val BASE_SUBJECT_NUMBER = 7
+        const val DIVIDER = 20201227
+    }
+
+    private val loop : Int = findLoop()
+
+    private fun findLoop(subjectNumber : Int = BASE_SUBJECT_NUMBER ) : Int {
+        var value = 1
+        var loop = 0
+        while (value != publicKey){
+            value *= subjectNumber
+            value %= DIVIDER
+            loop++
+        }
+        return loop
+    }
+
+    fun encrypt( key : Key ) : Long{
+        var encryptionKey = 1L
+        repeat(loop){
+            encryptionKey *= key.publicKey
+            encryptionKey %= DIVIDER
+        }
+        return encryptionKey
+    }
+
+}
