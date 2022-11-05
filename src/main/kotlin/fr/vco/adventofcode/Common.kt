bfcbb923a5fa60bd3ada176f1c3c69fdf7a0b303
Vincent <vincentcoubray@gmail.com>
2022-11-06 00:15:02 +0100

    Migrate from maven to gradle + split into submodules

--- src/main/kotlin/fr/vco/adventofcode/Common.kt
+++
@@ -1,7 +0,0 @@
-package fr.vco.adventofcode
-
-import java.io.InputStreamReader
-
-
-fun getInputReader(filePath : String) : InputStreamReader =
-    InputStreamReader(object {}.javaClass.getResource(filePath).openStream())
