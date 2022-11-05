e435aec361abcd1f5c33c5310eeb7d99417406a7
Vincent <vincentcoubray@gmail.com>
2022-11-06 00:58:48 +0100

    move advent-of-code-2021 into this repo

--- advent-of-code-2021/src/main/kotlin/fr/vco/aoc/y2021/Utils.kt
+++ advent-of-code-2021/src/main/kotlin/fr/vco/aoc/y2021/Utils.kt
@@ -2,7 +2,7 @@ package fr.vco.aoc.y2021
 
 import java.io.File
 
-private fun String.getInputFile() = File("src/main/resources", "$this.txt")
+private fun String.getInputFile() = File("advent-of-code-2021/src/main/resources", "$this.txt")
 fun readLines(fileName: String) = fileName.getInputFile().readLines()
 fun readFirstLine(fileName: String) = fileName.getInputFile().readLines().first()
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,6 @@ rootProject.name = "advent-of-code"
 
 include(
     "advent-of-code-2019",
-    "advent-of-code-2020"
+    "advent-of-code-2020",
+    "advent-of-code-2021"
 )
