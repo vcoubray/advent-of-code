52cd1d2bfe8b013e1883cfaec1c3ca8fe79bbe3b
Vincent <vincentcoubray@gmail.com>
2023-02-12 20:10:39 +0100

    2022 - Day 22 - Part 2

--- advent-of-code-2022/src/main/kotlin/fr/vco/aoc/y2022/Day22.kt
+++ advent-of-code-2022/src/main/kotlin/fr/vco/aoc/y2022/Day22.kt
@@ -3,11 +3,10 @@ package fr.vco.aoc.y2022
 fun main() {
 
     val input = readLines("Day22")
-
     val (map, instructions) = input.split { it.isBlank() }
 
-    println("Part 1 : ${MonkeyMap(map).play(instructions.joinToString(""))}")
-
+    println("Part 1 : ${Mapper(50, map).toFlatMap().execute(instructions.joinToString(""))}")
+    println("Part 2 : ${Mapper(50, map).toCubicMap().execute(instructions.joinToString(""))}")
 }
 
 enum class Direction(val dir: MonkeyMap.Position) {
@@ -17,46 +16,98 @@ enum class Direction(val dir: MonkeyMap.Position) {
     LEFT(MonkeyMap.Position(-1, 0))
 }
 
-class MonkeyMap(private val rawMap: List<String>) {
+class Mapper(private val panelSize: Int, private val rawMap: List<String>) {
     private val height = rawMap.size
     private val width = rawMap.firstOrNull()?.length ?: 0
-    private val tileMap = rawMap.mapIndexed { y, line ->
-        line.padEnd(width,' ').mapIndexed { x, it -> Tile(Position(x, y), it == '#') }
+
+    private val tiles = rawMap.mapIndexed { y, line ->
+        line.padEnd(width, ' ').mapIndexed { x, it -> MonkeyMap.Tile(MonkeyMap.Position(x, y), it) }
     }.flatten()
 
-    init {
-        println("$width $height")
-        println ("${tileMap.size}")
-        tileMap.forEach { tile ->
-            tile.neighbours = Direction.values().associateWith { getTile(getNextValidPos(tile.position, it)) }
+
+    fun toFlatMap() = toMonkeyMap(::wrapFlat)
+    fun toCubicMap() = toMonkeyMap(::wrapCubic)
+
+    private fun getRaw(pos: MonkeyMap.Position) = rawMap.getOrNull(pos.y)?.getOrNull(pos.x) ?: ' '
+    private fun getTile(pos: MonkeyMap.Position) = tiles[pos.y * width + pos.x]
+
+    private fun toMonkeyMap(wrapper: (MonkeyMap.Position, Direction) -> Pair<MonkeyMap.Position, Int>): MonkeyMap {
+        return MonkeyMap(tiles.filter { it.isValid }.map { tile ->
+            tile.apply {
+                neighbours = Direction.values().associateWith { getNeighbor(tile.position, it, wrapper) }
+            }
+        })
+    }
+
+    private fun getNeighbor(
+        pos: MonkeyMap.Position,
+        direction: Direction,
+        wrapper: (MonkeyMap.Position, Direction) -> Pair<MonkeyMap.Position, Int>,
+    ): Pair<MonkeyMap.Tile, Int> {
+        val (neighbor, rotation) = (pos + direction.dir to 0).takeIf { getRaw(it.first) != ' ' }
+            ?: run { wrapper(pos, direction) }
+        return getTile(neighbor) to rotation
+    }
+
+
+    private fun wrapFlat(pos: MonkeyMap.Position, direction: Direction): Pair<MonkeyMap.Position, Int> {
+        return when (direction) {
+            Direction.UP -> (0 until height).map { MonkeyMap.Position(pos.x, it) }.last { getRaw(it) != ' ' } to 0
+            Direction.DOWN -> (0 until height).map { MonkeyMap.Position(pos.x, it) }.first { getRaw(it) != ' ' } to 0
+            Direction.LEFT -> (0 until width).map { MonkeyMap.Position(it, pos.y) }.last { getRaw(it) != ' ' } to 0
+            Direction.RIGHT -> (0 until width).map { MonkeyMap.Position(it, pos.y) }.first { getRaw(it) != ' ' } to 0
         }
     }
 
-    fun getRaw(pos: Position) = rawMap.getOrNull(pos.y)?.getOrNull(pos.x)?:' '
-    fun isValid(pos: Position) = getRaw(pos) != ' '
-
-    fun getTile(pos: Position) = tileMap[pos.y * width + pos.x]
-
-    private fun getNextPos(pos: Position, direction: Direction): Position {
-        val next = pos + direction.dir
-        val nextX = next.x.mod(width)
-        val nextY = next.y.mod(height)
-        return Position(nextX, nextY)
-    }
-
-    private fun getNextValidPos(pos: Position, direction: Direction): Position {
-        var next = getNextPos(pos, direction)
-        while (getRaw(next) == ' ') {
-            next = getNextPos(next, direction)
+    //   1 2
+    //   4
+    // 6 7
+    // 9
+    private fun wrapCubic(pos: MonkeyMap.Position, direction: Direction): Pair<MonkeyMap.Position, Int> {
+        val panelX = pos.x % panelSize
+        val panelY = pos.y % panelSize
+        val cubePos = pos.y / panelSize * 3 + pos.x / panelSize
+        return when {
+            cubePos == 1 && direction == Direction.UP -> getPanelPos(9, 0, panelX) to 1
+            cubePos == 1 && direction == Direction.LEFT -> getPanelPos(6, 0, panelSize - panelY - 1) to 2
+            cubePos == 2 && direction == Direction.UP -> getPanelPos(9, panelX, panelSize - 1) to 0
+            cubePos == 2 && direction == Direction.RIGHT -> getPanelPos(7, panelSize - 1, panelSize - panelY - 1) to 2
+            cubePos == 2 && direction == Direction.DOWN -> getPanelPos(4, panelSize - 1, panelX) to 1
+            cubePos == 4 && direction == Direction.LEFT -> getPanelPos(6, panelY, 0) to -1
+            cubePos == 4 && direction == Direction.RIGHT -> getPanelPos(2, panelY, panelSize - 1) to -1
+            cubePos == 6 && direction == Direction.UP -> getPanelPos(4, 0, panelX) to 1
+            cubePos == 6 && direction == Direction.LEFT -> getPanelPos(1, 0, panelSize - panelY - 1) to 2
+            cubePos == 7 && direction == Direction.RIGHT -> getPanelPos(2, panelSize - 1, panelSize - panelY - 1) to 2
+            cubePos == 7 && direction == Direction.DOWN -> getPanelPos(9, panelSize - 1, panelX) to 1
+            cubePos == 9 && direction == Direction.RIGHT -> getPanelPos(7, panelY, panelSize - 1) to -1
+            cubePos == 9 && direction == Direction.DOWN -> getPanelPos(2, panelX, 0) to 0
+            cubePos == 9 && direction == Direction.LEFT -> getPanelPos(1, panelY, 0) to -1
+            else -> throw Exception("Should not happen")
         }
-        return next
     }
 
+    private fun getPanelPos(panelIndex: Int, x: Int, y: Int) =
+        MonkeyMap.Position(panelIndex % 3 * panelSize + x, panelIndex / 3 * panelSize + y)
 
-    fun getFirstState() = State(tileMap.first { isValid(it.position) })
+}
 
+class MonkeyMap(tiles: List<Tile>) {
+    private var tile = tiles.first { it.isValid }
+    private var directionIndex: Int = 0
+    private val directions = listOf(Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP)
 
-    fun String.toInstructionList(): List<String> {
+    fun execute(instructions: String): Int {
+        instructions.toInstructionList().forEach {
+            when (it) {
+                "R" -> rotate(1)
+                "L" -> rotate(-1)
+                else -> move(it.toInt())
+            }
+        }
+        return getPassword()
+    }
+
+    private fun String.toInstructionList(): List<String> {
         return buildList {
             var instruction = """(\d+|[RL])""".toRegex().find(this@toInstructionList)
             while (instruction != null) {
@@ -66,45 +117,30 @@ class MonkeyMap(private val rawMap: List<String>) {
         }
     }
 
-    fun play(instructions: String) : Int {
-        val state = getFirstState()
-        instructions.toInstructionList().forEach {
-            when (it) {
-                "R" -> state.rotate(1)
-                "L" -> state.rotate(-1)
-                else -> state.move(it.toInt())
+    private fun move(step: Int) {
+        repeat(step) {
+            val (next, rotate) = tile.neighbours[directions[directionIndex]]!!
+            if (!next.isWall) {
+                tile = next
+                rotate(rotate)
             }
         }
-        return state.getPassword()
     }
+
+    private fun rotate(rotate: Int) {
+        directionIndex = (directionIndex + rotate).mod(directions.size)
+    }
+
+    private fun getPassword() = 1000 * (tile.position.y + 1) + 4 * (tile.position.x + 1) + directionIndex
 
     data class Position(val x: Int, val y: Int) {
         operator fun plus(position: Position) = Position(x + position.x, y + position.y)
     }
 
-    class Tile(val position: Position, val isWall: Boolean) {
-        lateinit var neighbours: Map<Direction, Tile>
-    }
-
-    class State(
-        var tile: Tile,
-        var directionIndex: Int = 0,
-    ) {
-        private val directions = listOf(Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP)
-        fun move(step: Int) {
-            repeat(step) {
-                val next = tile.neighbours[directions[directionIndex]]!!
-                if (!next.isWall) {
-                    tile = next
-                }
-            }
-        }
-
-        fun rotate(rotate: Int) {
-            directionIndex = (directionIndex + rotate).mod(directions.size)
-        }
-
-        fun getPassword() = 1000 * (tile.position.y+1) + 4 * (tile.position.x+1) + directionIndex
+    class Tile(val position: Position, type: Char) {
+        val isWall = type == '#'
+        val isValid = type != ' '
+        lateinit var neighbours: Map<Direction, Pair<Tile, Int>>
     }
 
 }
