3407212131bfaa7104facbbd293fac6c8187b70a
Vincent <vincentcoubray@gmail.com>
2023-12-01 20:31:59 +0100

    2019 - Day 18 - Part 2

--- advent-of-code-2019/src/main/kotlin/fr/vco/adventofcode/y2019/Day18.kt
+++ advent-of-code-2019/src/main/kotlin/fr/vco/adventofcode/y2019/Day18.kt
@@ -5,12 +5,13 @@ import java.util.PriorityQueue
 fun main() {
     val input = getInputReader("/inputDay18.txt")
 
-    val maze = Maze(input.readLines())
-    println("Part 1 : ${resolve(maze)}")
+    val mazeBuilder = MazeBuilder(input.readLines())
+
+    println("Part 1 : ${mazeBuilder.build().resolve()}")
+    println("Part 2 : ${mazeBuilder.buildPart2().resolve()}")
 }
 
-class Maze(grid: List<String>) {
-
+class MazeBuilder(grid: List<String>) {
     companion object {
         private val directions = listOf(
             Position(1, 0),
@@ -20,35 +21,49 @@ class Maze(grid: List<String>) {
         )
     }
 
-    val height = grid.size
-    val width = grid.first().length
-    val cells = grid.joinToString("")
-    val neighbors = MutableList(width * height) { emptyList<Int>() }
-    val entities = mutableMapOf<Char, Int>()
-    val distances: Map<Char, MutableMap<Char, Int>>
-    val reachable: Map<Char, Set<Char>>
+    private val height = grid.size
+    private val width = grid.first().length
+    private var cells = grid.joinToString("")
+    private val startId = cells.indexOf('@')
+    private val neighbors = MutableList(width * height) { emptyList<Int>() }
 
-    init {
+    fun build(): Maze {
+        val entities = mutableMapOf<Char, Int>()
         cells.forEachIndexed { i, cell ->
             if (cell.isEntity()) entities[cell] = i
-            if (cell != '#') neighbors[i] = getNeighbors(i).map { it.y * width + it.x }
+            if (cell.isWall()) neighbors[i] = emptyList()
+            else neighbors[i] = getNeighbors(i).map { it.y * width + it.x }
         }
-        distances = entities.keys.associateWith { entities.keys.associateWith { -1 }.toMutableMap() }
-        reachable = entities.map { (entity, pos) -> entity to computeReachableEntities(pos) }.toMap()
 
-        entities.forEach { (entity, id) ->
-            computeDistance(entity, id)
-        }
+        val reachable = entities.map { (entity, pos) -> entity to computeReachableEntities(pos) }.toMap()
+        val distances = entities.map { (entity, pos) -> entity to computeDistance(entity, pos) }.toMap()
+
+        return Maze(setOf('@'), entities, distances, reachable)
     }
+    
+    fun buildPart2(): Maze {
+        val cells = this.replaceEntrance()
+
+        val entities = mutableMapOf<Char, Int>()
+        cells.forEachIndexed { i, cell ->
+            if (cell.isEntity()) entities[cell] = i
+            if (cell.isWall()) neighbors[i] = emptyList()
+            else neighbors[i] = getNeighbors(i).map { it.y * width + it.x }
+        }
+
+        val reachable = entities.map { (entity, pos) -> entity to computeReachableEntities(pos) }.toMap()
+        val distances = entities.map { (entity, pos) -> entity to computeDistance(entity, pos) }.toMap()
+
+        return Maze(setOf('1', '2', '3', '4'), entities, distances, reachable)
+    }
+
 
     private fun getNeighbors(id: Int): List<Position> {
-        val pos = Position(id % width, id / width)
+        val pos = id.toPosition()
         return directions.map { pos + it }
             .filter { it.x in 0 until width && it.y in 0 until height }
-            .filter { cells[id] != '#' }
+            .filter { !cells[id].isWall() }
     }
-
-    private fun Char.isEntity() = this != '#' && this != '.'
 
     private fun computeReachableEntities(startId: Int): Set<Char> {
         val toVisit = ArrayDeque<Int>().apply { add(startId) }
@@ -72,15 +87,16 @@ class Maze(grid: List<String>) {
         return reachable
     }
 
-    private fun computeDistance(startEntity: Char, startId: Int ) {
+    private fun computeDistance(startEntity: Char, startId: Int): Map<Char, Int> {
         val toVisit = ArrayDeque<Int>().apply { add(startId) }
         val visited = MutableList(neighbors.size) { -1 }
         visited[startId] = 0
-        distances[startEntity]!![startEntity] = 0
+
+        val distances = mutableMapOf(startEntity to 0)
         while (toVisit.isNotEmpty()) {
             val current = toVisit.removeFirst()
             if (cells[current].isEntity() && current != startId) {
-                distances[startEntity]!![cells[current]] = visited[current]
+                distances[cells[current]] = visited[current]
             }
             neighbors[current]
                 .filter { visited[it] == -1 }
@@ -89,67 +105,83 @@ class Maze(grid: List<String>) {
                     toVisit.add(it)
                 }
         }
+        return distances
     }
+    
+    private fun replaceEntrance() = cells
+        .replaceRange(startId - width - 1..startId - width + 1, "1#2")
+        .replaceRange(startId - 1..startId + 1, "###")
+        .replaceRange(startId + width - 1..startId + width + 1, "3#4")
 
-    fun getStartState() = State(
-        this,
-        '@',
-        reachable['@']!!,
-        entities.keys.associateWith { false }.toMutableMap().apply { this['@'] = true }
-    )
+
+    private fun Int.toPosition() = Position(this % width, this / width)
+    private fun Char.isEntity() = this != '#' && this != '.'
+    private fun Char.isWall() = this == '#'
 }
 
-class State(
-    val maze : Maze,
-    val current: Char,
-//    val distances: Map<Char, MutableMap<Char, Int>>,
-    val reachable: Set<Char>,
+
+class Maze(
+    val starts: Set<Char>,
+    val entities: Map<Char, Int>,
+    val distances: Map<Char, Map<Char, Int>>,
+    val reachable: Map<Char, Set<Char>>,
+) {
+
+    private fun getStartState() = MazeState(
+        starts.toList(),
+        starts.map { reachable[it]!! },
+        entities.keys.associateWith { false }.toMutableMap().apply { this['@'] = true }
+    )
+    
+    fun resolve(): Int {
+        val start = getStartState()
+        val toVisit = PriorityQueue(compareBy<MazeState> { it.steps }).apply { add(start) }
+        val visited = mutableMapOf(start.id to start)
+
+        while (toVisit.isNotEmpty()) {
+            val current = toVisit.poll()
+            if (current.isFinal()) {
+                return current.steps
+            }
+            nextStates(current).forEach { state ->
+                if ((visited[state.id]?.steps ?: Int.MAX_VALUE) > state.steps) {
+                    toVisit.add(state)
+                    visited[state.id] = state
+                }
+            }
+        }
+        return -1
+    }
+
+    private fun nextStates(state: MazeState): List<MazeState> {
+        return state.reachable.mapIndexed { i, reachable ->
+            reachable.filter { entity -> state.visited[entity] == false }
+                .filter { entity -> entity.isLowerCase() || state.visited[entity.toLowerCase()] == true }
+                .map { target ->
+                    MazeState(
+                        state.current.toMutableList().apply { this[i] = target },
+                        state.reachable.toMutableList().apply { this[i] = this[i] + this@Maze.reachable[target]!! },
+                        state.visited.toMutableMap().apply { this[target] = true },
+                        state.steps + distances[state.current[i]]!![target]!!,
+                        state.path + target
+                    )
+                }
+        }.flatten()
+    }
+
+}
+
+class MazeState(
+    val current: List<Char>,
+    val reachable: List<Set<Char>>,
     val visited: Map<Char, Boolean>,
     val steps: Int = 0,
     val path: String = "$current",
 ) {
-
-    val id = current + "_" + visited.filter { (k, v) -> v && k.isLowerCase() }.keys.joinToString("")
-
-    fun nextStates(): List<State> {
-
-        return reachable
-            .filter { entity -> visited[entity] == false}
-            .filter { entity -> entity.isLowerCase() || visited[entity.toLowerCase()] == true }
-            .map { target ->
-                State(
-                    maze,
-                    target,
-                    reachable + maze.reachable[target]!!,
-                    visited.toMutableMap().apply { this[target] = true },
-                    steps + maze.distances[current]!![target]!!,
-                    path + target
-                )
-            }
-
-    }
-
+    val id = current.joinToString("") + "_" + visited.filter { (k, v) -> v && k.isLowerCase() }.keys.joinToString("")
     fun isFinal() = visited.none { (k, v) -> k.isLowerCase() && !v }
 }
 
-fun resolve(maze: Maze): Int {
 
-    val start = maze.getStartState()
-    val toVisit = PriorityQueue(compareBy<State> { it.steps }).apply { add(start) }
-    val visited = mutableMapOf(start.id to start)
 
-    while (toVisit.isNotEmpty()) {
-        val current = toVisit.poll()
-        if (current.isFinal()) {
-            return current.steps
-        }
-        current.nextStates().forEach { state ->
-            if ((visited[state.id]?.steps ?: Int.MAX_VALUE) > state.steps) {
-                toVisit.add(state)
-                visited[state.id] = state
-            }
-        }
-    }
-    return -1
-}
 
