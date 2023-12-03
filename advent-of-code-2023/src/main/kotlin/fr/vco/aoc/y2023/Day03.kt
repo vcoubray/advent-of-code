1113776c665ab62a18ab1247bc76bc17629f24e3
Vincent <vincentcoubray@gmail.com>
2023-12-03 20:53:34 +0100

    2023 - Day 03 - Part 2

--- advent-of-code-2023/src/main/kotlin/fr/vco/aoc/y2023/Day03.kt
+++ advent-of-code-2023/src/main/kotlin/fr/vco/aoc/y2023/Day03.kt
@@ -1,37 +1,60 @@
 package fr.vco.aoc.y2023
 
-typealias EngineSchematic = List<String>
-
 fun main() {
-    val schema = readLines("Day03")
-    val numbers = schema.flatMapIndexed { i, it -> it.getPartNumbers(i) }
+    val input = readLines("Day03")
+    val schema = EngineSchematic(input)
 
-    println("Part 1: ${numbers.filter { schema.isPartNumber(it) }.sumOf { it.number }}")
+    println("Part 1: ${schema.partNumberSum()}")
+    println("Part 2: ${schema.gearRatio()}")
 }
-
 
 data class PartNumber(
     val number: Int,
-    val line: Int,
-    val range: IntRange,
+    val y: Int,
+    val xRange: IntRange,
+    var symbols: Map<Char, Int> = emptyMap(),
 )
 
-fun EngineSchematic.isPartNumber(number: PartNumber): Boolean {
-    return (number.line - 1..number.line + 1).any { y ->
-        (number.range.first - 1..number.range.last + 1).any { x ->
-            isSymbol(x, y)
-        }
+class EngineSchematic(
+    private val schema: List<String>,
+) {
+    private val width = schema.firstOrNull()?.length ?: 0
+    private val partNumbers = schema.flatMapIndexed { i, it -> it.getPartNumbers(i) }
+
+    init {
+        partNumbers.forEach { it.symbols = it.findSymbols() }
     }
-}
 
-fun EngineSchematic.isSymbol(x: Int, y: Int): Boolean {
-    val c = this.getOrNull(y)?.getOrNull(x)
-    return c != null && !c.isDigit() && c != '.'
-}
+    fun partNumberSum() = partNumbers.filter { it.symbols.isNotEmpty() }.sumOf { it.number }
 
+    fun gearRatio(): Int {
+        val gears = mutableMapOf<Int, List<Int>>()
+        partNumbers.forEach { number ->
+            number.symbols.filter { (k, _) -> k == '*' }
+                .map { (_, v) -> v to number.number }
+                .forEach { (id, number) ->
+                    gears[id] = (gears[id] ?: listOf()) + number
+                }
+        }
+
+        return gears.filter { (_, numbers) -> numbers.size == 2 }
+            .values.sumOf { (a, b) -> a * b }
+    }
+
+
+    private fun PartNumber.findSymbols(): Map<Char, Int> {
+        return (y - 1..y + 1).flatMap { y ->
+            (xRange.first - 1..xRange.last + 1).mapNotNull { x ->
+                schema.get(x, y)?.takeIf { it.isSymbol() }?.let { it to y * width + x }
+            }
+        }.toMap()
+    }
+
+    private fun List<String>.get(x: Int, y: Int) = this.getOrNull(y)?.getOrNull(x)
+    private fun Char.isSymbol() = !this.isDigit() && this != '.'
+}
 
 fun String.getPartNumbers(lineId: Int): List<PartNumber> {
-
     val numbers = mutableListOf<PartNumber>()
     var currentNumber = ""
     var numberId = -1
@@ -42,7 +65,6 @@ fun String.getPartNumbers(lineId: Int): List<PartNumber> {
             if (numberId == -1) numberId = i
         } else {
             if (numberId != -1) {
-
                 numbers.add(PartNumber(currentNumber.toInt(), lineId, numberId until i))
                 numberId = -1
                 currentNumber = ""
