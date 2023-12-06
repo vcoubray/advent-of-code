6a450bb8921b6ca207de73f8d66d73fd2fed3901
vcoubray <vcoubray@sedona.fr>
2023-12-06 14:57:10 +0100

    2023 - Day 06

--- advent-of-code-2023/src/main/kotlin/fr/vco/aoc/y2023/Day06.kt
+++ advent-of-code-2023/src/main/kotlin/fr/vco/aoc/y2023/Day06.kt
@@ -8,6 +8,6 @@ fun main() {
     val races = times.zip(distances).map{(time, distance) -> time to distance}
     println("Part 1: ${races.map {(time, distance) -> (0..time).count{it * (time - it) > distance} }.fold(1){a,b -> a*b}}")
 
-    val (time, distance) = input.map{it.filter{it.isDigit()}.toLong()}
+    val (time, distance) = input.map{it.filter{c -> c.isDigit()}.toLong()}
     println("Part 2: ${(0..time).count{it * (time - it) > distance}}")
 }
