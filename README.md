dd0c24eb9571ac317f37c5c98293758577c84d3b
Vincent <vincentcoubray@gmail.com>
2023-12-08 00:27:31 +0100

    Move AocPlugin in buildSrc

--- README.md
+++ README.md
@@ -1,9 +1,45 @@
 # AdventOfCode
 
-My solutions for the Advent of code challenges : https://adventofcode.com
+My solutions in kotlin for the Advent of code challenges : https://adventofcode.com
 
-* [Advent of Code 2019](./advent-of-code-2019) in Kotlin v1.4.20
-* [Advent of Code 2020](./advent-of-code-2020) in Kotlin v1.4.20
-* [Advent of Code 2021](./advent-of-code-2021) in Kotlin v1.6.0
-* [Advent of Code 2022](./advent-of-code-2022) in Kotlin v1.7.21
-* [Advent of Code 2023](./advent-of-code-2023) in Kotlin v1.9.21
+* [Advent of Code 2019](./advent-of-code-2019)
+* [Advent of Code 2020](./advent-of-code-2020)
+* [Advent of Code 2021](./advent-of-code-2021)
+* [Advent of Code 2022](./advent-of-code-2022)
+* [Advent of Code 2023](./advent-of-code-2023)
+
+## Retrieve personal inputs
+
+The precompiled `aoc-plugin` plugin allow to retrieve automatically the personal inputs of a specified year.
+
+Don't forget to exclude these inputs file from the repository with `.gitignore` – we should not post them publicly, as [Eric Wastl requested for](https://twitter.com/ericwastl/status/1465805354214830081).
+
+### Apply plugin
+
+```kts
+// build.gradle.kts
+plugins {
+    `aoc-inputs`
+}
+
+apply<Aoc_inputs_gradle.AocPlugin>()
+```
+
+### Configure the plugin
+
+```kts
+configure<Aoc_inputs_gradle.AocExtension> {
+    year = 2023
+    dest = "${project.projectDir}/src/main/resources/inputs"
+    session = "<AOC_SESSION_ID>"
+}
+```
+The plugin will automatically add all available inputs files for the specified `year` in the folder `dest`.
+The `session` can be retrieve from the cookies of your browser after login in adventofcode.com
+
+
+### Usage
+```shell
+./gradlew <module>:loadAocInputs
+```
+if already present, the inputs files will be overridden 
---
+++ buildSrc/src/main/kotlin/aoc-inputs.gradle.kts
@@ -0,0 +1,58 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.provider.Property
+import org.gradle.kotlin.dsl.*
+import java.io.File
+import java.io.IOException
+import java.net.URL
+import java.time.LocalDate
+
+interface AocExtension {
+    val year: Property<Int>
+    val dest: Property<String>
+    val session: Property<String>
+}
+
+class AocPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        val extension = project.extensions.create<AocExtension>("aoc-inputs")
+        project.extensions.add("aocInputs", extension)
+        project.task("loadAocInputs") {
+            doLast {
+                val dest = File(extension.dest.get())
+
+                // Create inputs folder if not already exists
+                dest.mkdirs()
+
+                val maxDate = listOf(
+                    LocalDate.now(),
+                    LocalDate.of(extension.year.get(), 12, 25)
+                )
+                    .minOrNull()!!
+                println(maxDate)
+                val maxDay = maxDate.dayOfMonth
+
+                try {
+                    (1..maxDay).forEach { day ->
+                        val con =
+                            URL("https://adventofcode.com/${extension.year.get()}/day/$day/input").openConnection()
+                        con.setRequestProperty("Cookie", "session=${extension.session.get()}")
+                        con.inputStream.bufferedReader().use { reader ->
+                            val dayName = day.toString().padStart(2, '0')
+                            val inputFile = File("${dest.absolutePath}/Day${dayName}.txt")
+                            inputFile.writeText(reader.readText())
+                        }
+                    }
+                } catch (e: IOException) {
+                    if (e.message?.startsWith("Server returned HTTP response code: 400") == true) {
+                        println("The session is probably not valid. Try to get a new one.")
+                    }
+                    throw e
+                }
+
+            }
+        }
+    }
+}
+
+
