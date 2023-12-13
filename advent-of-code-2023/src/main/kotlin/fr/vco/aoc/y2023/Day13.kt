1cf6f2853e95f708eef46057f611a042965373fc
vcoubray <vcoubray@sedona.fr>
2023-12-13 14:20:34 +0100

    2023 - Day 13 - Part 2

--- advent-of-code-2023/src/main/kotlin/fr/vco/aoc/y2023/Day13.kt
+++ advent-of-code-2023/src/main/kotlin/fr/vco/aoc/y2023/Day13.kt
@@ -3,29 +3,34 @@ package fr.vco.aoc.y2023
 fun main() {
     val input = readLines("Day13")
     val patterns = input.split { it.isBlank() }
+        .map(::MirrorPattern)
 
-    println("Part 1: ${patterns.sumOf{it.reflectionScore()}}")
-
+    println("Part 1: ${patterns.sumOf { it.reflectionScore() }}")
+    println("Part 2: ${patterns.sumOf { it.reflectionScore(1) }}")
 }
 
-fun List<String>.reflectionScore(): Int {
-    return findHorizontalReflection() * 100 + findVerticalReflection()
-}
+class MirrorPattern(mirrors: List<String>) {
 
-fun List<String>.findHorizontalReflection(): Int {
-    for (i in 0..<this.size - 1) {
-        var reflective = true
-        for (j in 0..<Math.min(i+1, size-i-1)) {
-            if (this[i+j+1] != this[i-j]) {
-                reflective = false
-                break
-            }
-        }
-        if(reflective) return i+1
+    private val horizontalDiffs = mirrors.mapToHorizontalReflectionDiffs()
+    private val verticalsDiffs = mirrors.mapToVerticalReflectionDiffs()
+
+    fun reflectionScore(diff: Int = 0): Int {
+        return (horizontalDiffs.indexOf(diff) + 1) * 100 + (verticalsDiffs.indexOf(diff) + 1)
     }
-    return 0
-}
 
-fun List<String>.findVerticalReflection(): Int {
-    return transpose(this).findHorizontalReflection()
+    private fun List<String>.mapToHorizontalReflectionDiffs(): List<Int> {
+        return (0..this.size - 2).map { i ->
+            var currentDiff = 0
+            for (j in 0..<(i + 1).coerceAtMost(size - i - 1)) {
+                currentDiff += this[i + j + 1].countDiff(this[i - j])
+            }
+            currentDiff
+        }
+    }
+
+    private fun List<String>.mapToVerticalReflectionDiffs(): List<Int> {
+        return transpose(this).mapToHorizontalReflectionDiffs()
+    }
+
+    private fun String.countDiff(other: String) = this.filterIndexed { i, it -> it != other[i] }.length
 }
