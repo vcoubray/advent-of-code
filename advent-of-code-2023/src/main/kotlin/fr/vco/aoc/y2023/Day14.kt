e26cf961a12e266359d7942eae87faad7af43b6a
vcoubray <vcoubray@sedona.fr>
2023-12-14 15:03:29 +0100

    2023 - Day 14 - Part 2

--- advent-of-code-2023/src/main/kotlin/fr/vco/aoc/y2023/Day14.kt
+++ advent-of-code-2023/src/main/kotlin/fr/vco/aoc/y2023/Day14.kt
@@ -2,9 +2,9 @@ package fr.vco.aoc.y2023
 
 fun main() {
     val input = readLines("Day14")
-    val panel = ControlPanel(input)
 
-    println("Part 1 : ${panel.moveNorth().score()}")
+    println("Part 1 : ${ControlPanel(input).moveNorth().score()}")
+    println("Part 2 : ${ControlPanel(input).spin(1_000_000_000)}")
 }
 
 class ControlPanel(input: List<String>) {
@@ -12,16 +12,42 @@ class ControlPanel(input: List<String>) {
     val width = grid.first().size
     val height = grid.size
 
+    val logs = mutableListOf(gridToString())
+    val logScores = mutableListOf(score())
+
+    fun spin(repetition: Int = 1) : Int {
+        var i = 1
+        while (i <= repetition) {
+            moveNorth()
+            moveWest()
+            moveSouth()
+            moveEast()
+            val log = gridToString()
+            val logId = logs.indexOf(log)
+            if (logId != -1) {
+                val loopSize = i - logId
+                val remainingSpins = repetition - i
+                val finalId = (remainingSpins % loopSize) + logId
+                return logScores[finalId]
+            }
+            logs.add(gridToString())
+            logScores.add(score())
+            i++
+        }
+        return 0
+    }
+
+    private fun gridToString() = grid.joinToString("") { it.joinToString("") }
+
     fun moveNorth() = apply {
-        for (x in 0..<width){
+        for (x in 0..<width) {
             var freeSpace = 0
             for (y in 0..<height) {
-                when(grid[y][x]) {
-                    '#' -> freeSpace = y+1
+                when (grid[y][x]) {
+                    '#' -> freeSpace = y + 1
                     'O' -> {
                         grid[y][x] = '.'
                         grid[freeSpace][x] = 'O'
-
                         freeSpace += 1
                     }
                 }
@@ -29,6 +55,53 @@ class ControlPanel(input: List<String>) {
         }
     }
 
-    fun score() = grid.foldIndexed(0) { i, score, line -> score + (line.count { c -> c == 'O' } * (height - i)) }
+    fun moveSouth() = apply {
+        for (x in 0..<width) {
+            var freeSpace = height - 1
+            for (y in height - 1 downTo 0) {
+                when (grid[y][x]) {
+                    '#' -> freeSpace = y - 1
+                    'O' -> {
+                        grid[y][x] = '.'
+                        grid[freeSpace][x] = 'O'
+                        freeSpace -= 1
+                    }
+                }
+            }
+        }
+    }
 
+    fun moveWest() = apply {
+        for (y in 0..<height) {
+            var freeSpace = 0
+            for (x in 0..<width) {
+                when (grid[y][x]) {
+                    '#' -> freeSpace = x + 1
+                    'O' -> {
+                        grid[y][x] = '.'
+                        grid[y][freeSpace] = 'O'
+                        freeSpace += 1
+                    }
+                }
+            }
+        }
+    }
+
+    fun moveEast() = apply {
+        for (y in 0..<height) {
+            var freeSpace = width - 1
+            for (x in width - 1 downTo 0) {
+                when (grid[y][x]) {
+                    '#' -> freeSpace = x - 1
+                    'O' -> {
+                        grid[y][x] = '.'
+                        grid[y][freeSpace] = 'O'
+                        freeSpace -= 1
+                    }
+                }
+            }
+        }
+    }
+
+    fun score() = grid.foldIndexed(0) { i, score, line -> score + (line.count { c -> c == 'O' } * (height - i)) }
 }
