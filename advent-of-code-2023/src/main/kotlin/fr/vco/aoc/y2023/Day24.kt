55f879a89778ee976d829d7d8bf8e2cae4835b85
vcoubray <vcoubray@sedona.fr>
2023-12-26 14:36:53 +0100

    2023 - Day 24 - Part 2

--- advent-of-code-2023/src/main/kotlin/fr/vco/aoc/y2023/Day24.kt
+++ advent-of-code-2023/src/main/kotlin/fr/vco/aoc/y2023/Day24.kt
@@ -5,15 +5,15 @@ import java.math.BigInteger
 
 fun main() {
     val input = readLines("Day24")
-
-    println("Part 1: ${input.map { it.toHailStone() }.countIntersectXY(200_000_000_000_000L..400_000_000_000_000L)}")
-
+    val hailStones = input.map { it.toHailStone() }
+    println("Part 1: ${hailStones.countIntersectXY(200_000_000_000_000L..400_000_000_000_000L)}")
+    println("Part 2: ${hailStones.findRockPos()}")
 }
 
 data class HailStone(
-        val x: BigInteger, val y: BigInteger, val z: BigInteger,
-        val vx: BigInteger, val vy: BigInteger, val vz: BigInteger
-    ) {
+    val x: BigInteger, val y: BigInteger, val z: BigInteger,
+    val vx: BigInteger, val vy: BigInteger, val vz: BigInteger
+) {
 
     fun intersectXY(h: HailStone): Pair<BigDecimal, BigDecimal>? {
         if (vx * h.vy == vy * h.vx) return null
@@ -25,6 +25,18 @@ data class HailStone(
             x.toBigDecimal() + (t * vx) to y.toBigDecimal() + (t * vy)
         } else null
     }
+
+    fun moveToRelativeReferential(vx: Int = 0, vy: Int = 0, vz: Int = 0) = copy(
+        vx = this.vx - vx.toBigInteger(),
+        vy = this.vy - vy.toBigInteger(),
+        vz = this.vz - vz.toBigInteger()
+    )
+
+    fun getHailStoneAt(timing: BigInteger) = copy(
+        x = x + (vx * timing),
+        y = y + (vy * timing),
+        z = z + (vz * timing)
+    )
 }
 
 fun List<HailStone>.countIntersectXY(area: LongRange): Int {
@@ -39,6 +51,36 @@ fun List<HailStone>.countIntersectXY(area: LongRange): Int {
         }
     }
     return crossingCount
+}
+
+fun List<HailStone>.findRockPos(): BigInteger {
+
+    for (vx in -1000..1000) {
+        for (vy in -1000..1000) {
+
+            val (a, b) = this.take(2).map { it.moveToRelativeReferential(vx, vy) }
+            val rPos = a.intersectXY(b)
+
+            if (rPos != null && this.drop(2).all { rPos == a.intersectXY(it.moveToRelativeReferential(vx, vy)) }) {
+                val timingA = (rPos.first.toBigInteger() - a.x) / a.vx
+                val timingB = (rPos.first.toBigInteger() - b.x) / b.vx
+
+                val interA = first().getHailStoneAt(timingA)
+                val interB = get(1).getHailStoneAt(timingB)
+
+                val rvx = (interA.x - interB.x) / (timingA - timingB)
+                val rvy = (interA.y - interB.y) / (timingA - timingB)
+                val rvz = (interA.z - interB.z) / (timingA - timingB)
+
+                val rx = interA.x - (rvx * timingA)
+                val ry = interA.y - (rvy * timingA)
+                val rz = interA.z - (rvz * timingA)
+
+                return rx + ry + rz
+            }
+        }
+    }
+    return -BigInteger.ONE
 }
 
 fun String.toHailStone(): HailStone {
