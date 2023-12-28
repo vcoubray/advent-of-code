7aa92e70f0cd6875a4d4df2af71e19b6f7f9b13d
Vincent <vincentcoubray@gmail.com>
2023-12-28 19:24:07 +0100

    2023 - Day 21 - Part 2

--- advent-of-code-2023/src/main/kotlin/fr/vco/aoc/y2023/Day21.kt
+++ advent-of-code-2023/src/main/kotlin/fr/vco/aoc/y2023/Day21.kt
@@ -2,43 +2,60 @@ package fr.vco.aoc.y2023
 
 fun main() {
     val input = readLines("Day21")
-        
+
     println("Part 1: ${GardenMaze(input).bfs(64)}")
+    println("Part 2: ${GardenMaze(input).getPolynomial().resolve(26501365)}")
 }
 
-class GardenMaze(input: List<String>) {
-    val width = input.first().length
-    val height = input.size
+class GardenMaze(private val maze: List<String>) {
+    private val width = maze.first().length
+    private val height = maze.size
+    private val start = findStart()
 
-    val maze = input.joinToString("")
-    val start = maze.indexOfLast { it == 'S' }
-
-    val neighbors = List(width * height) { i ->
-        val pos = Position(i % width, i / width)
-        Direction.entries.map { pos + it.vector }
-            .filter { it.x in 0..<width && it.y in 0..<height }
-            .map { it.y * width + it.x }
-            .filter { maze[it] == '.' }
+    private fun findStart(): Position {
+        val y = maze.indexOfFirst { 'S' in it }
+        val x = maze[y].indexOfFirst { it == 'S' }
+        return Position(x, y)
     }
 
+    private fun Position.getNeighbours() =
+        Direction.entries.map { this + it.vector }
+            .filter { (x, y) -> maze[y.mod(height)][x.mod(width)] != '#' }
+
     fun bfs(maxStep: Int): Int {
-        val toVisit = ArrayDeque<Int>().apply { addLast(start) }
-        val visited = MutableList(width * height) { -1 }
-        visited[start] = 0
+        val toVisit = ArrayDeque<Position>().apply { addLast(start) }
+        val visited = mutableMapOf(start to 0)
 
         while (toVisit.isNotEmpty()) {
             val current = toVisit.removeFirst()
-            if (visited[current] >= maxStep) continue
+            if (visited[current]!! >= maxStep) continue
 
-            neighbors[current].filter { visited[it] == -1 }
+            current.getNeighbours().filter { it !in visited }
                 .forEach {
-                    visited[it] = visited[current] + 1
+                    visited[it] = visited[current]!! + 1
                     toVisit.add(it)
                 }
         }
-
-        return visited.filterNot { it == -1 }
+        return visited.values.filterNot { it == -1 }
             .count { it % 2 == maxStep % 2 }
     }
-
+    
+    fun getPolynomial(): Polynomial {
+        val x = width / 2
+        val y = x + width
+        val z = y + width
+        
+        val X = bfs(x).toDouble()
+        val Y = bfs(y).toDouble()
+        val Z = bfs(z).toDouble()
+        
+        val a = (x * (Z - Y) + y * (X - Z) + z * (Y - X)) / ((x - y) * (x - z) * (y - z))
+        val b = (X - Y) / (x - y) - a * (x + y)
+        val c = X - a * x * x - b * x
+        return Polynomial(a, b, c)
+    }
 }
+
+data class Polynomial(private val a: Double, private val b: Double, private val c: Double) {
+    fun resolve(x: Long): Long = (a * x * x + b * x + c).toLong()
+} 
