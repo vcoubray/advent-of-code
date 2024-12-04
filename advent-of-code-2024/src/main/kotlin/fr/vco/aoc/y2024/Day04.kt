5cfec1ea1d3f04e5c1eaea017218106908702bb5
vcoubray <vcoubray@sedona.fr>
2024-12-04 11:44:03 +0100

    2024 - Day 04

---
+++ advent-of-code-2024/src/main/kotlin/fr/vco/aoc/y2024/Day04.kt
@@ -0,0 +1,58 @@
+package fr.vco.aoc.y2024
+
+fun main() {
+    val input = readLines("Day04")
+
+    println("Part 1 : ${input.countWord("XMAS")}")
+    println("Part 2 : ${input.countXWord("MAS")}")
+}
+
+val DIRECTIONS = listOf(
+    Position(-1, -1),
+    Position(-1, 0),
+    Position(-1, 1),
+    Position(0, -1),
+    Position(0, 1),
+    Position(1, -1),
+    Position(1, 0),
+    Position(1, 1),
+)
+
+val DIAGONALS = listOf(
+    Position(0, 0) to Position(1, 1),
+    Position(0, 1) to Position(1, -1),
+    Position(1, 0) to Position(-1, 1),
+    Position(1, 1) to Position(-1, -1)
+)
+
+fun List<String>.countWord(word: String): Int {
+    return indices.sumOf { y ->
+        (0..this[y].lastIndex).sumOf { x ->
+            this.countWord(word, Position(x, y))
+        }
+    }
+}
+
+fun List<String>.countWord(word: String, pos: Position): Int {
+    return DIRECTIONS.count { dir -> this.hasWord(word, pos, dir) }
+}
+
+fun List<String>.countXWord(word: String): Int {
+    return indices.sumOf { y ->
+        (0..this[y].lastIndex).count { x ->
+            this.hasXWord(word, Position(x, y))
+        }
+    }
+}
+
+fun List<String>.hasXWord(word: String, pos: Position): Boolean {
+    return DIAGONALS.map { (start, dir) -> (pos + start * (word.length-1)) to dir }
+        .count { (start, dir) -> hasWord(word, start, dir) } == 2
+}
+
+fun List<String>.hasWord(word: String, pos: Position, direction: Position): Boolean {
+    return (0..word.lastIndex)
+        .map { i -> i to pos + direction * i }
+        .all { (i, pos) -> word[i] == this.getOrNull(pos.y)?.getOrNull(pos.x) }
+}
+
--- advent-of-code-2024/src/main/kotlin/fr/vco/aoc/y2024/Utils.kt
+++ advent-of-code-2024/src/main/kotlin/fr/vco/aoc/y2024/Utils.kt
@@ -5,3 +5,7 @@ import java.io.File
 private fun String.getInputFile() = File("advent-of-code-2024/src/main/resources/inputs", "$this.txt")
 fun readLines(fileName: String) = fileName.getInputFile().readLines()
 
+data class Position(val x: Int, val y: Int) {
+    operator fun plus(other: Position) = Position(x + other.x, y + other.y)
+    operator fun times(value: Int) = Position(x * value, y * value)
+}
