2b4080337f2fc16c86ab850892f2159db4f2b6c1
vcoubray <vcoubray@sedona.fr>
2025-12-01 17:22:24 +0100

    2025 - Day 01

--- advent-of-code-2025/src/main/kotlin/fr/vco/aoc/y2025/Day01.kt
+++ advent-of-code-2025/src/main/kotlin/fr/vco/aoc/y2025/Day01.kt
@@ -1,8 +1,21 @@
 package fr.vco.aoc.y2025
 
+
+import kotlin.math.max
+import kotlin.math.min
+
 fun main() {
     val input = readLines("Day01")
+    val numbers = input
+        .map { it.toRotation() }
+        .fold(listOf(50)) { numbers, rotation -> numbers + (rotation + numbers.last()) }
 
-    println("Part 1: ")
-    println("Part 2: ")
+    println("Part 1: ${numbers.count { it % 100 == 0 }}")
+    println("Part 2: ${numbers.windowed(2).sumOf {(a, b) -> countMultipleOf100(a, b)} - numbers.count { it % 100 == 0 }}")
 }
+
+fun String.toRotation() = this.replace("L", "-")
+    .replace("R", "")
+    .toInt()
+
+fun countMultipleOf100(a: Int, b: Int) = (min(a, b)..max(a, b)).count{it % 100 == 0}
