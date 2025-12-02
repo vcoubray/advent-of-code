06b76473472d5e3168ee1fb1fac5efb69fbbe1b5
vcoubray <vcoubray@sedona.fr>
2025-12-02 17:44:55 +0100

    2025 - Day 02

---
+++ advent-of-code-2025/src/main/kotlin/fr/vco/aoc/y2025/Day02.kt
@@ -0,0 +1,22 @@
+package fr.vco.aoc.y2025
+
+fun main() {
+    val input = readLines("Day02").first()
+
+    val ranges = input.split(",")
+        .map { it.split("-") }
+        .map { (start, end) -> start.toLong()..end.toLong() }
+
+    println("Part 1: ${ranges.flatMap { getInvalidIds(it, 2) }.sum()}")
+    println("Part 2: ${ranges.flatMap { getInvalidIds(it) }.sum()}")
+}
+
+fun getInvalidIds(range: LongRange, maxDivisor: Int? = null): List<Long> {
+    return (range).filter {
+        val value = it.toString()
+        (2..(maxDivisor?:value.length)).any { divisor ->
+            if (value.length % divisor != 0) false
+            else value.chunked(value.length/divisor).toSet().size == 1
+        }
+    }
+}
